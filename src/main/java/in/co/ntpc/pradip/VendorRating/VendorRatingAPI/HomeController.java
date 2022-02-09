3099d44196b38afa2e6fd951a73222057973a2d3
susnitdgp <susnitdgp@gmail.com>
2022-02-09 06:46:53 +0000

    Home Controller Updated

---
+++ src/main/java/in/co/ntpc/pradip/VendorRating/VendorRatingAPI/HomeController.java
@@ -0,0 +1,21 @@
+package in.co.ntpc.pradip.VendorRating.VendorRatingAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+    @GetMapping("/v1/GetEmployee")
+	public String GetEmployee() {
+		return "Get Employee List";
+	}
+
+
+}
